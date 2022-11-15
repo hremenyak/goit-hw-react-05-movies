6ced896b70fe98526f84580f31baa1949d360d7a
hremenyak <hremenyak@gmail.com>
2022-11-15 19:00:35 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,6 @@
 import { getTrendingMovies } from 'services/api';
 import { GlobalStyle } from './GlobalStyle';
 export const App = () => {
-  const result = async () => {
-    const result = await getTrendingMovies();
-    return result;
-  };
-
-  const value = result();
   return (
     <>
       <div>React homework template</div>
