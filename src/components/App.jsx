7d05e8c416f698a28b117ed07efe553c1c8b3fda
hremenyak <hremenyak@gmail.com>
2022-11-15 19:10:46 +0100

    working with api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,13 @@
-// import { getTrendingMovies } from 'services/api';
+import { getTrendingMovies } from 'services/api';
 import { GlobalStyle } from './GlobalStyle';
 export const App = () => {
+  const result = async () => {
+    const result = await getTrendingMovies();
+    return result;
+  };
+
+  const value = result();
+
   return (
     <>
       <div>React homework template</div>
--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 const KEY = 'fb0f8889c4de169f6f8eba7a95a6c733';
 
-// export const getTrendingMovies = async () => {
-//   const response = await axios.get(`/trending/all/day?api_key=${KEY}`);
-//   console.log(response.data.results);
-//   return response;
-// };
+export const getTrendingMovies = async () => {
+  const response = await axios.get(`/trending/all/day?api_key=${KEY}`);
+  console.log(response.data.results);
+  return response;
+};
